1d4503b460ccdc177977708d0bba3fb00fbad6c5
ravi8470 <rvkmr0851@gmail.com>
2019-05-04 13:03:28 +0530

    added some topics and changed articles table id column data type

--- src/schemas/migrations/createTable.sql
+++ src/schemas/migrations/createTable.sql
@@ -21,3 +21,19 @@ url VARCHAR NOT NULL,
 title VARCHAR,
 topic_id INTEGER REFERENCES topics(id)
 );
+
+--Sources Table
+--Will contain URLs from which feed topics' data is fetched.
+-- CREATE TABLE sources(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR NOT NULL UNIQUE,
+--     baseURL VARCHAR, --baseURL for api calls.
+--     topic_ids integer[] --which all topics this source can serve
+-- );
+
+--Populating TOpics table
+INSERT INTO topics(name) VALUES ('javascript'), ('coding'), ('Economics'), ('marketing'), ('environment'), ('history'), ('law'), ('math'),('Space'),('pokemon');
+
+--Changing article_id to string so reddit ids can be used
+ALTER TABLE articles ALTER COLUMN id TYPE VARCHAR;
+ALTER TABLE articles ALTER COLUMN id DROP DEFAULT;--removing the auto-inc due to using the SERIAL type
