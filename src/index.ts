5753342d87e98cd3c2b56de6743a349abdf67c93
ravi8470 <rvkmr0851@gmail.com>
2019-05-04 17:57:59 +0530

    used doenv config, added cors for all preflight requests, graphql middleware for authentication, passing context to graphql, catch-all not found routes

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,88 @@
 import "reflect-metadata";
 //import graphql related imports here
+import { buildTypeDefsAndResolvers } from "type-graphql";
+import TopicResolver from './Resolvers/TopicResolver';
+import { makeExecutableSchema } from "graphql-tools";
 
 import express from "express";
+const graphqlHTTP = require('express-graphql');
 import cors from "cors";
 import bodyParser from "body-parser";
+const util = require('util');
 import axios from "axios";
 import * as testStuff from "../utilities/testFile";
+import UserResolver from "./Resolvers/UserResolver";
+const routerp = require("./routes/newRouter");
 const dotenv = require('dotenv');
-const { Pool } = require('pg')
-const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
-async function bootstrap() {
-    dotenv.config();
+// const { populateArticles } = require('./tasks/serverTasks');
+
+
+dotenv.config();
+
+var myHeaders = {
+    'Content-Type': 'application/json',
+    'app_key': process.env.AVIANA_API_KEY
+}
+
+async function bootstrap(){
+    // populateArticles();
     var app: express.Application = express();
-
     app.use(cors());
-    app.use(bodyParser.urlencoded());
+    app.options('*', cors())//handling all preflight requests.
+    app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
-
-    const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
-    console.log(connectionString);
-    const pool = new Pool({ connectionString: connectionString});
-
-
-    app.get('/', (req, res) => {
-        res.send('Hello');
-    });
-
-    app.post('/register', (req,resx) => {
-        console.log('Email: ' + req.body.password);
-        var myHeaders = {
-            'Content-Type': 'application/json',
-            'app_key': process.env.AVIANA_API_KEY
-        }
-        var data = {
-            'email' : req.body.email,
-            'password': req.body.password
-        }
-        axios.post('https://aviana.herokuapp.com/user/signup', data, {headers: myHeaders}).then(
-            res => {
-                console.log(res.data);
-                // console.log(res.data.email);
-                // console.log(res.data.success);
-                resx.send({some:'got', more: res.data});
+     
+    app.use('/graphql', async function (req, res, next) {
+        console.log('Request URL:', req.originalUrl);
+        if(req.body.token){
+            var result = await axios.post('https://aviana.herokuapp.com/user/verify', {'accessToken': req.body.token}, {headers: myHeaders}).catch(err =>{
+                console.log('error occurred:' + err);
+                res.send({error: err});
+            });
+            if(result.data._id){
+                req.body.id = result.data._id;
+                req.body.email = result.data.email;
+                console.log(req.body.id);
+                next();
             }
-        )
+            else{
+                res.send({error: 'Session has expired, Please re-login.'});
+            }
+        }
+        else{
+            res.send({error:'Error in authentication Server.'});
+        }
         
     });
 
-    app.post('/login',(req,resx) => {
-        var myHeaders = {
-            'Content-Type': 'application/json',
-            'app_key': process.env.AVIANA_API_KEY
-        }
-        var data = {
-            'email' : req.body.email,
-            'password': req.body.password
-        }
-        var tokenHeader;
-        axios.post('https://aviana.herokuapp.com/user/signin', data, {headers: myHeaders}).then(res => {
-            // console.log(res.data.token);
-            tokenHeader = {'accessToken': res.data.token};
-            axios.post('https://aviana.herokuapp.com/user/verify', {'accessToken': res.data.token}, {headers: tokenHeader})
-            .then(resz => {
-                pool.query('SELECT * FROM users WHERE id = $1', [resz.data._id], (err, res) => {
-                    if (err) {throw err}
-                    console.log('res.rowCount ' + res.rowCount);
-                    if(res.rowCount == 0){
-                        console.log('inserting user');
-                        pool.query('INSERT INTO users(id, email) VALUES ($1, $2)', [resz.data._id, resz.data.email], 
-                            (err, res) => {
-                                if(err) throw err;
-                                console.log('user inserted');
-                                resx.send({auth: true, token: tokenHeader.accessToken, email: resz.data.email});
-                        })
-                    }
-                    else{
-                        resx.send({auth: true, token: tokenHeader.accessToken, email: resz.data.email});
-                    }
-                })
-                console.log('after data inserted');
-            }).catch(err => console.log('error in retrieving user details' + err));
-        }).catch(err => console.log('error in getting access token:' + err));
+    const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
+        resolvers: [TopicResolver, UserResolver],
     });
 
+    
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+    
+    
+
+    app.use('/', require("./routes/authRouter"));
+
+    app.use('/graphql', graphqlHTTP((req: any) =>({
+        schema: schema,
+        graphiql: true,
+        context: {
+            id: req.body.id
+        }
+    })));
+
+    app.use('/user', routerp);
+    app.all('*', (req,res) => {
+        res.send("<h1>404 - Not Found </h1>");
+    });
+
+
     app.listen(process.env.PORT, () => console.log(`server started at ${process.env.PORT}`));
-
 }
+bootstrap(); 
 
-bootstrap();
+
