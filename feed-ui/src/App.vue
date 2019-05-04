3b0c2d15a5448e0d9d3389427ec3adb8de76a06c
ravi8470 <rvkmr0851@gmail.com>
2019-05-04 12:55:47 +0530

    added logout impl and checks for displaying certain links only when user is loggedin

--- feed-ui/src/App.vue
+++ feed-ui/src/App.vue
@@ -2,13 +2,35 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/register">Register</router-link>|
-      <router-link to="/login">Login</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link v-if="!checkLogin()" to="/register">|Register</router-link>
+      <router-link v-if="!checkLogin()" to="/login">|Login</router-link>
+      <router-link v-if="checkLogin()" to="/dashboard">|Dashboard</router-link>
+      <a v-if="checkLogin()" @click="logout" class="router-link">|Logout</a>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default   {
+  methods: {
+    logout(){
+      localStorage.removeItem('jwt');
+      localStorage.removeItem('email');
+      const h = this.$createElement;
+      this.$notify({
+        message: h('b', { style: 'color: green' }, 'Successfully Logged out.')
+      });
+      this.$router.push('/');
+    },
+    checkLogin(){
+      return localStorage.getItem('email') != null;
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
