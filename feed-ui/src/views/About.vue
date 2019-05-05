f6b00ba7f584a64fbc202e1369988b8ce2a6de9d
ravi8470 <rvkmr0851@gmail.com>
2019-05-05 21:33:01 +0530

    implemented about page

--- feed-ui/src/views/About.vue
+++ feed-ui/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Personalised Feed App</h1>
+    <h2>Built With:</h2>
+    <img style="width: 100px; height: 100px" src="../assets/graphql_icon.png"/><br>
+    <img style="width: 100px; height: 100px" src="../assets/element_icon.svg"/><br>
+    <img style="width: 200px; height: 100px" src="../assets/nodejs_icon.png"/><br>
+    <img style="width: 100px; height: 100px" src="../assets/typescript.png"/><br>
+    <img style="width: 100px; height: 100px" src="../assets/Vue_icon.png"/>
   </div>
 </template>
+
+
