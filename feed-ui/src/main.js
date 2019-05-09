49bf96fb3b6bfdf7002ba277db4e6333b310ecfc
ravi8470 <rvkmr0851@gmail.com>
2019-05-09 22:39:12 +0530

    Implemented component import for element-ui instead of entire lib

--- feed-ui/src/main.js
+++ feed-ui/src/main.js
@@ -1,14 +1,65 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from '../node_modules/element-ui';
-import '../node_modules/element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en';
+// import ElementUI from '../node_modules/element-ui';
+// import '../node_modules/element-ui/lib/theme-chalk/index.css';
+// import locale from 'element-ui/lib/locale/lang/en';
 import Axios from 'axios'
 
+import {
+  Dialog,
+  Menu,
+  MenuItem,
+  Input,
+  Checkbox,
+  CheckboxButton,
+  Select,
+  Button,
+  ButtonGroup,
+  Form,
+  FormItem,
+  Icon,
+  Row,
+  Col,
+  Card,
+  Carousel,
+  CarouselItem,
+  Container,
+  Header,
+  Footer,
+  Main,
+  Notification,
+  Loading,
+} from 'element-ui';
+
+Vue.use(Dialog);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Input);
+Vue.use(Checkbox);
+Vue.use(CheckboxButton);
+Vue.use(Select);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Card);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Footer);
+Vue.use(Main);
+Vue.use(Loading);
+
+Vue.prototype.$notify = Notification;
+
 Vue.prototype.$http = Axios;
-Vue.config.productionTip = false
-Vue.use(ElementUI, { locale, size: 'medium', zIndex: 3000 });
+Vue.config.productionTip = false;
+// Vue.use(ElementUI, { locale, size: 'medium', zIndex: 3000 });
 
 new Vue({
   router,
